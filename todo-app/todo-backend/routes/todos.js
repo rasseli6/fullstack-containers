dee6d5c7a555c8f56c41f3d8d94eafd76038c23a
Rasmus Harmanen <rasmus@harmanen.fi>
2026-05-18 16:53:32 +0300

    tehtava 7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,17 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  req.todo.text = req.body.text;
+  req.todo.done = req.body.done;
+
+  const updatedTodo = await req.todo.save();
+
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
